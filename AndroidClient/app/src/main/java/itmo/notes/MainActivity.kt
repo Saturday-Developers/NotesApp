79a9dca1bdc04eadd73b61102cd536b22072b424
Tatiana Pestova <tanya.tom106@gmail.com>
2020-04-26 16:44:51 +0300

    Added icons for emotions and weather

--- AndroidClient/app/src/main/java/itmo/notes/MainActivity.kt
+++ AndroidClient/app/src/main/java/itmo/notes/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import itmo.notes.activities.ChartsActivity
+import itmo.notes.activities.TestEmojiActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -14,10 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         buttonViewCharts.text = "Charts"
+        buttonViewTestEmoji.text = "Show icons"
     }
 
     fun viewCharts(view: View) {
         val intent = Intent(this, ChartsActivity::class.java).apply {}
+        startActivity(intent)
+    }
+
+    fun viewEmoji(view: View) {
+        val intent = Intent(this, TestEmojiActivity::class.java).apply {}
         startActivity(intent)
     }
 }
---
+++ AndroidClient/app/src/main/java/itmo/notes/activities/TestEmojiActivity.kt
@@ -0,0 +1,13 @@
+package itmo.notes.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import itmo.notes.R
+
+class TestEmojiActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_emoji)
+    }
+}
